ecb8828f1c178188821c590a1c9376d28e0eff21
aweibuome <131541176+aweibuome@users.noreply.github.com>
2023-09-16 10:25:44 +0800

    Update main.go

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ import (
 	//                          vvvvvvvvvvvvvv                          //
 	//                               vvvv                               //
 
-	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ahsai"            // ahsai tts
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ahsai"            // ahsai tts
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aifalse"          // 服务器监控
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aipaint"          // ai绘图
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aiwife"           // 随机老婆
@@ -257,7 +257,7 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	 sus = append(sus, 3185727122)
+	 sus = append(sus, 1164927406)
 	// sus = append(sus, 87654321)
 
 	// 启用 webui
